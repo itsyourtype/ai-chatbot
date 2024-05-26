133009d4ee1d18611b30a51160e3c233be619656
itsyourtype <166209542+itsyourtype@users.noreply.github.com>
2024-05-26 16:16:12 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,10 +12,10 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{' '}
+      XezaAI built with{' '}
       <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://github.com/vercel/ai">
-        Vercel AI SDK
+      <ExternalLink href="https://www.xeza.in">
+        XezaDev
       </ExternalLink>
       .
     </p>
